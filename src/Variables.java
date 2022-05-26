aa975b031cb46c7f48a38cda9116b2c1ab5d5ebd
yflorescastrejon <106184715+yflorescastrejon@users.noreply.github.com>
2022-05-26 14:57:36 -0400

    Update Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -1,8 +1,8 @@
 /**
  * class name: Variables.java
- * author: ________
- * date:
- * version:
+ * author: Yeslin Flores-Castrejon
+ * date: May 26, 2022
+ * version: 1.0
  * description: This program will display summary of primitive data types.
  * source: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
  * direction: 1) read the source material
@@ -15,6 +15,19 @@ public class Variables
     public static void main(String[] args)
     {
         System.out.println("Summary of The Primitive Data Types");
+        System.out.println("A byte is an 8-bit that consists of an 0 and 1.");
+        System.out.println("It can have a minimum of 128 and a maximum of 127.");
+        System.out.println("Byte data can even be used to save memory!");
+        System.out.println("The short data type is a 16 bit that has two integers (0 or 1). It can have a minimum of -32,768 and a maximum of 32,767.");
+        System.out.println("When you need to save memory a short can also be used.");
+        System.out.println("An int data type is a 32 bit that also has two integers.It can have a minimum of -231 and a maximum value of 231-1.");
+        System.out.println("In Java you can use the int data type to represent a 32-bit unsigned integer.");
+        System.out.printin("A long data type is a 64-bit two's complement integer. It can have a minimum of value of -263 and a maximum value of 263-1.");
+        System.out.println("In Java you can use the lomg data typr to represent a 64-bit unsigned interger in order to create a range of values wider than those provided to you.");
+        System.out.println("A float data type is a 32-bit IEEE 754 floating point.To save memory of floating point numbers, you can use a float.");
+        System.out.println("However, the float data type cannot be used for precise values, for example, currency.");
+        System.out.println("A double data type is a 64-bit IEEE 754 floating point.");
+                         
     }
 
 }
