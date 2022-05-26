b40287e53ab062e2a564fd76776b6d4096e371f5
hpark4 <hpark4@gmail.com>
2022-05-26 00:41:25 -0400

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,20 @@
+/**
+ * class name: Variables.java
+ * author: ________
+ * date:
+ * version:
+ * description: This program will display summary of primitive data types.
+ * source: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+ * direction: 1) read the source material
+ *            2) summary of primitive data types: byte, short, int, long, float,
+ *            double, boolean, char (2 lines per each type)
+ *
+ */
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Summary of The Primitive Data Types");
+    }
+
+}
